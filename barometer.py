c9696f0293a353a66bfab8d7000c704558eba00e
uktechreviews <spencerorgan1@me.com>
2019-08-26 19:30:58 +0100

    Initial commit

---
+++ barometer.py
@@ -0,0 +1,17 @@
+from sense_emu import SenseHat
+from time import sleep
+sense = SenseHat()
+r=[255,0,0]
+sense.clear()
+
+while True:
+    for c in range (0,8):
+        pressure = sense.pressure
+        print (pressure)
+        graph_pressure = int(pressure / 150)
+        print (graph_pressure)
+        for i in range(graph_pressure):
+            sense.set_pixel(c,i,r)
+        print (c)
+	sleep(300) 
+    sense.clear()
