dcbe537d935d80030f72d117494b69f26f308d03
uktechreviews <spencerorgan1@me.com>
2019-08-28 13:21:59 +0100

    New version added

---
+++ barometer_pycon_better.py
@@ -0,0 +1,41 @@
+#Spencer Organ - August 2019
+
+from sense_hat import SenseHat
+from time import sleep
+sense = SenseHat()
+r=[255,0,0]
+g=[0,255,0]
+b=[0,0,255]
+wait = 180
+sense.clear(0,0,0)
+
+while True:
+    for c in range (0,8):
+        pressure = sense.pressure
+        print (pressure)
+        graph_pressure = 0
+        if pressure >=970 and pressure <=990.9:
+            print ("Stormy weather")
+            colour = b
+            graph_pressure =2
+        if pressure >=991 and pressure <=999.9:
+            print ("Rain on the way")
+            colour = b
+            graph_pressure =4
+        if pressure >=1000 and pressure <=1020.9:
+            print ("Normal")
+            colour = g
+            graph_pressure =5
+        if pressure >=1021 and pressure <=1030.9:
+            print ("Fair")
+            colour = r
+            graph_pressure =6
+        if pressure >=1031 and pressure <=1060.9:
+            print ("Very dry")
+            colour = r
+            graph_pressure =6
+            
+        for i in range(graph_pressure):
+            sense.set_pixel(c,i,colour)
+        sleep(wait)
+    sense.clear()
